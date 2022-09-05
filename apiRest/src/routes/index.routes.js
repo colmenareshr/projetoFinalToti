06a57dd0a67b85c56339d090a61b50458157f1d1
Humberto Colmenares <colmenareshr@gmail.com>
2022-09-05 17:23:44 -0300

    Limpiando codigo en las rutas

--- apiRest/src/routes/index.routes.js
+++ apiRest/src/routes/index.routes.js
@@ -5,30 +5,26 @@ const router = Router()
 
 //Liste todos os produtos
 router.get('/api/produtos', async (req, res) =>{
+  const produtos = await Produto.find()
   try {
-    const produtos = await Produto.find()
     res.status(200).json(produtos)
-    console.log(produtos);
-  } catch (error) {
-    res.status(500).end()
-  console.log('Consulta inválida');  
+  } catch (err) {
+    res.status(400).end()
   }
-  
 })
 //liste um produto só
 router.get('/api/produtos/:id', async (req, res) => {
   const { id } = req.params
-  const produtos =  await Produto.findById(id)
-try {
-  res.status(200).json(produtos)
-} catch (error) {
-  console.error(error);
-  res.status(400).end()
-}
+  const produtos = await Produto.findById(id)
+  try {
+    res.status(200).json(produtos)
+  } catch (error) {
+    res.status(400).end
+  } 
 })
 //Adicionar um novo produto
-router.post('/api/cachorros/brinquedo', async (req, res) =>{
-  // const produto = Produto(req.body)
+router.post('/api/cachorros/brinquedo', (req, res) =>{
+  const produto = Produto(req.body)
   const produtoSaved = Produto.save()
   console.log(produtoSaved);
 })
@@ -38,7 +34,11 @@ router.post('/api/cachorros/brinquedo', async (req, res) =>{
 router.delete('/api/produtos/:id', async (req, res) => {
   const { id } = req.params
   await Produto.findByIdAndDelete(id)
-  res.status(204).end()
+  try {
+    res.status(204).end()
+  } catch (err) {
+    console.error(err);
+  }
 })
 
 module.exports = router;
